d5ea49c1b0acbecbf4d720c901c36e9fd8154013
Raj Lalith <rajlalith1234@gmail.com>
2025-05-09 11:46:28 -0700

    public/contact.html

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -130,7 +130,7 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      { id: user._id, email: user.email, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
